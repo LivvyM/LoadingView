654135104ab269ced0f225e5b579f7b9aa035715
livvy <lvm@kingnet.com>
2017-03-14 15:03:30 +0800

    Initial commit

---
+++ app/src/main/java/cc/livvy/loadingview/test/MainActivity.java
@@ -0,0 +1,31 @@
+package cc.livvy.loadingview.test;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import cc.livvy.loadingview.R;
+
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+	public void showSimple(View view) {
+		startActivity(new Intent(getApplicationContext(), SimpleActivity.class));
+	}
+
+	public void showExample1(View view) {
+		startActivity(new Intent(getApplicationContext(), Example1Activity.class));
+	}
+
+	public void showExample2(View view) {
+		startActivity(new Intent(getApplicationContext(), Example2Activity.class));
+	}
+
+}
